254c5e9811cc517e533f8adcf3e0449df1771a8a
Aryan <aryanchandrakar2403@gmail.com>
2026-08-20 12:42:58 +0530

    New Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Library for standard input/output functions
+
+int main() {
+    // Prints the string to the console
+    int x = 1;
+    printf("Hello, World!\n"); 
+    printf("%d",x);
+    
+    // Terminates the main function successfully
+    return 0; 
+}
